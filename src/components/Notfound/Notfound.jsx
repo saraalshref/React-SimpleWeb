4318fd1d2b1f2d8175536288fbc2d82c39da564f
saraalshref <saraelshref12@gmail.com>
2024-06-16 04:28:14 +0300

    add style to Notfound page

--- src/components/Notfound/Notfound.jsx
+++ src/components/Notfound/Notfound.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
 
-function Notfound() {
-  return (
-    <div>
-      Notfound
-    </div>
-  )
-}
+const Notfound = () => {
+    return (
+        <div className='text-center mt-5'>
+            <h1>404 - Page Not Found</h1>
+            <img className='w-50 h-auto' src='https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif' 
+            alt='Animated GIF'/>
 
-export default Notfound
+            <p>The page you are looking for does not exist.</p>
+        </div>
+    );
+};
+
+export default Notfound;
